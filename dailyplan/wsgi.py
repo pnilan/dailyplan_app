cde821f51b2fa153f200b5c5ddeb01df185c32ee
pnilan <nilan.patrick@gmail.com>
2023-02-22 17:11:39 -0800

    fixed CSS on index

--- dailyplan/wsgi.py
+++ dailyplan/wsgi.py
@@ -2,10 +2,6 @@ from dailyplan import create_app
 from dotenv import load_dotenv
 
 load_dotenv('.env')
-<<<<<<< HEAD
-=======
-
->>>>>>> 04df0c7e7147433b7aa429858bf1c2d1272f1129
 
 app = create_app()
 
